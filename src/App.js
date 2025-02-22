d2f28470f2490e865aeb8a827323a7f0250be03e
ChennyToo <kchencd1@gmail.com>
2025-02-22 14:45:18 -0500

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { getCryptoBars } from './MarketData'; // Adjust the path if needed
 
 function App() {
+  const [bars, setBars] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getCryptoBars(); // Call the Alpaca service
+        console.log(data)
+        setBars(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
